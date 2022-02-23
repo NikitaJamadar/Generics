4f29e52a22f45a6673b6c7206fa310924ea828b7
nikhita <nikhitajamadar1998@gmail.com>
2022-02-23 12:42:00 +0530

    Find maximum integer value

---
+++ UC1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int maxNumber= Program.FindmaxIntnumber(10, 40, 20);
+            Console.WriteLine("Maximum number is :"+maxNumber);
+        }
+        public static int FindmaxIntnumber(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num3) > 0 && num2.CompareTo(num1) > 0)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
