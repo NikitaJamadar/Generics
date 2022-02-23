cf8ae90a6e53eb7a4283b699d893a793b694cb2a
nikhita <nikhitajamadar1998@gmail.com>
2022-02-23 15:31:54 +0530

    Find maximum value using generics method

---
+++ Refactor1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Refactor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine($"Maximum integer number is {Program.FindMaxValue<int>(111,666,333)}");
+            Console.WriteLine($"Maximum float number is {Program.FindMaxValue<double>(5.5, 4.4, 3.3)}");
+            Console.WriteLine($"Maximum string is {Program.FindMaxValue<string>("Apple","Banana","Peach")}");
+        }
+        public static T FindMaxValue<T>(T FirstValue, T secondValue, T thirdValue)
+        {
+            if (Comparer<T>.Default.Compare(FirstValue, secondValue) > 0 && Comparer<T>.Default.Compare(FirstValue, thirdValue) > 0)
+            {
+                return FirstValue;
+            }
+            else if (Comparer<T>.Default.Compare(secondValue, FirstValue) > 0 && Comparer<T>.Default.Compare(secondValue, thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
