fc10ff645fdcd38366becc17a1a29be710a90e9b
nikhita <nikhitajamadar1998@gmail.com>
2022-02-23 15:32:59 +0530

    Find maximum value using generics class

---
+++ Refactor2/GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor2
+{
+    class GenericClass<T>
+    {
+        public static T FindMaxValue<T>(T FirstValue, T secondValue, T thirdValue)
+        {
+            if (Comparer<T>.Default.Compare(FirstValue, secondValue) > 0 && Comparer<T>.Default.Compare(FirstValue, thirdValue) > 0)
+            {
+                return FirstValue;
+            }
+            else if (Comparer<T>.Default.Compare(secondValue, FirstValue) > 0 && Comparer<T>.Default.Compare(secondValue, thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
---
+++ Refactor2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Refactor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Maximum integer number is {GenericClass<int>.FindMaxValue<int>(111, 666, 333)}");
+            Console.WriteLine($"Maximum float number is {GenericClass<double>.FindMaxValue<double>(5.5, 4.4, 3.3)}");
+            Console.WriteLine($"Maximum string is {GenericClass<string>.FindMaxValue<string>("Apple", "Banana", "Peach")}");
+        }
+       
+    }
+}
