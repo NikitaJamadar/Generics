77355202d75b10253f5baadfac1e37d6daf73369
nikhita <nikhitajamadar1998@gmail.com>
2022-02-23 15:30:40 +0530

    Find maximum value using generics method

---
+++ UC5/MaxCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC5
+{
+    class MaxCount<T>
+    {
+        
+        public T[] Values;
+       
+        public MaxCount(params T[] Values)
+        {
+            this.Values = Values;
+        }
+      
+        public T[] SortValues(T[] Values)
+        {
+            Array.Sort(Values);
+            return Values;
+        }
+       
+        public T TestMaximum(T[] values)
+        {
+            var maxValue = SortValues(Values);
+            return maxValue[maxValue.Length-1];
+        }
+        public void PrintMax()
+        {
+            Console.WriteLine("max value:" + TestMaximum(Values));
+        }
+       
+    }
+}
+
---
+++ UC5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MaxCount<int> intValue = new MaxCount<int>(27, 52, 48, 73, 36);
+            intValue.PrintMax();
+
+            MaxCount<float> floatValue = new MaxCount<float>(64.2f, 28.3f, 83.3f, 73.4f, 93.3f);
+            floatValue.PrintMax();
+
+            MaxCount<string> stringValue = new MaxCount<string>("N", "K", "S", "R", "J");
+            stringValue.PrintMax();
+        }
+    }
+}
