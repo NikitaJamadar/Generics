c72a296563b309f38841a4e62f15a1d5dc5faaf8
nikhita <nikhitajamadar1998@gmail.com>
2022-02-23 15:29:21 +0530

    Find maximum value of more than three parameters

---
+++ UC4/MaxCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4
+{
+    class MaxCount<T>
+    {
+        public T[] ValuesInArray;
+
+        public MaxCount(params T[] ValuesInArray)
+        {
+            this.ValuesInArray = ValuesInArray;
+        }
+		
+		public  void MaxValue()
+		{
+            Array.Sort(this.ValuesInArray);
+          
+            Console.WriteLine("max value:" +this.ValuesInArray[ValuesInArray.Length - 1]);
+		}
+	}
+}
---
+++ UC4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+			MaxCount<int> intValue = new MaxCount<int>(27, 52, 48, 73, 36);
+			intValue.MaxValue();
+
+            MaxCount<float> floatValue = new MaxCount<float>(64.2f, 28.3f, 83.3f, 73.4f, 93.3f);
+            floatValue.MaxValue();
+
+            MaxCount<string> stringValue = new MaxCount<string>("N", "K", "S", "R", "J");
+            stringValue.MaxValue();
+        }
+    }
+}
